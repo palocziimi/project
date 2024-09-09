c65efdbc781cafbaeec96fc9272ca0e003a0a4c9
Pálóczi Imre <palocziimi@gmail.com>
2024-09-09 14:19:58 +0200

    first element

---
+++ function.js
@@ -0,0 +1,7 @@
+function getFirstElement (arr){
+    return arr[0];
+}
+const numbers = [1, 2, 3];
+const words = ["apple", "banana", "cherry"];
+console.log(`First number: ${getFirstElement(numbers)}`);
+console.log(`First word: ${getFirstElement(words)}`);
